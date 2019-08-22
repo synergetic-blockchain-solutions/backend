cb703b17813701b05b6ff916fa935cf230a03bb4
Nick Spain <nicholas.spain96@gmail.com>
2019-08-22 22:32:36 +1000

    Configure CORS request to be allowed from any origin

--- src/main/kotlin/com/synergeticsolutions/familyartefacts/SecurityConfig.kt
+++ src/main/kotlin/com/synergeticsolutions/familyartefacts/SecurityConfig.kt
@@ -1,17 +1,22 @@
 package com.synergeticsolutions.familyartefacts
 
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
-@Configuration
+@EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
-        http.csrf().disable()
+        http
+            .cors()
+            .and().csrf().disable()
     }
 
     /**
@@ -20,5 +25,15 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
+    }
+
+    /**
+     * Cross-Origin Resource Sharing config. This is a REST API so just allow everything.
+     */
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", CorsConfiguration().applyPermitDefaultValues())
+        return source
     }
 }
