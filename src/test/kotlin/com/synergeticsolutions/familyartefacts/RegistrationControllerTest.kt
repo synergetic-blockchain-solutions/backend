b1a998199407ae13d984a2153e9a125f4e52a6f8
Nick Spain <nicholas.spain96@gmail.com>
2019-08-22 21:07:33 +1000

    Use WebTestClient not MockMvc
    WebTestClient runs in a more similar environment to the actaul application
    and handles things like exceptions being thrown in the same manner as
    real life.

--- src/test/kotlin/com/synergeticsolutions/familyartefacts/RegistrationControllerTest.kt
+++ src/test/kotlin/com/synergeticsolutions/familyartefacts/RegistrationControllerTest.kt
@@ -1,81 +1,136 @@
 package com.synergeticsolutions.familyartefacts
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.containsInAnyOrder
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.http.MediaType
+import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@AutoConfigureWebTestClient
 class RegistrationControllerTest {
 
     @Autowired
     lateinit var mockMvc: MockMvc
 
-    val mapper = ObjectMapper().registerKotlinModule()
+    @Autowired
+    lateinit var client: WebTestClient
+
+    @Autowired
+    lateinit var userRepository: UserRepository
+
+    @BeforeEach
+    fun clearRepository() {
+        userRepository.deleteAll()
+    }
 
     @Test
     fun `it should not allow blank names`() {
         val registrationRequest = RegistrationRequest("", "example@example.com", "secret", "secret")
-        mockMvc.perform(post("/register")
+        client.post().uri("/register")
             .contentType(MediaType.APPLICATION_JSON_UTF8)
-            .content(mapper.writeValueAsString(registrationRequest)))
-            .andExpect(status().isBadRequest)
+            .syncBody(registrationRequest)
+            .exchange()
+            .expectStatus().isBadRequest
+            .expectBody()
+            .jsonPath("$.message").value(`is`("Validation failed"))
+            .jsonPath("$.errors").value(containsInAnyOrder("'name' must not be blank"))
     }
 
     @Test
     fun `it should not allow invalid emails`() {
         val registrationRequest = RegistrationRequest("name", "example", "secret", "secret")
-        mockMvc.perform(post("/register")
+        client.post().uri("/register")
             .contentType(MediaType.APPLICATION_JSON_UTF8)
-            .content(mapper.writeValueAsString(registrationRequest)))
-            .andExpect(status().isBadRequest)
+            .syncBody(registrationRequest)
+            .exchange()
+            .expectStatus().isBadRequest
+            .expectBody()
+            .jsonPath("$.message").value(`is`("Validation failed"))
+            .jsonPath("$.errors").value(containsInAnyOrder("'email' must be a well-formed email address"))
     }
 
     @Test
-    fun `it should not allow blank password or confirm passwords`() {
-        val registrationRequest = RegistrationRequest("", "example@example.com", "", "")
-        mockMvc.perform(post("/register")
+    fun `it should not allow short password or confirm passwords`() {
+        val registrationRequest = RegistrationRequest("name", "example@example.com", "", "")
+        client.post().uri("/register")
             .contentType(MediaType.APPLICATION_JSON_UTF8)
-            .content(mapper.writeValueAsString(registrationRequest)))
-            .andExpect(status().isBadRequest)
+            .syncBody(registrationRequest)
+            .exchange()
+            .expectStatus().isBadRequest
+            .expectBody()
+            .jsonPath("$.message").value(`is`("Validation failed"))
+            .jsonPath("$.errors").value(
+                containsInAnyOrder(
+                    "'password' must have at least 6 characters",
+                    "'confirmPassword' must have at least 6 characters"
+                )
+            )
     }
 
     @Test
     fun `it should not allow different password and confirm passwords`() {
-        val registrationRequest = RegistrationRequest("", "example@example.com", "secret1", "secret2")
-        mockMvc.perform(post("/register")
+        val registrationRequest = RegistrationRequest("name", "example@example.com", "secret1", "secret2")
+        client.post().uri("/register")
             .contentType(MediaType.APPLICATION_JSON_UTF8)
-            .content(mapper.writeValueAsString(registrationRequest)))
-            .andExpect(status().isBadRequest)
+            .syncBody(registrationRequest)
+            .exchange()
+            .expectStatus().isBadRequest
+            .expectBody()
+            .jsonPath("$.message").value(`is`("Validation failed"))
+            .jsonPath("$.errors").value(containsInAnyOrder("'password' and 'confirmPassword' are not matching"))
+    }
+
+    @Test
+    fun `it should return all validation errors`() {
+        val registrationRequest = RegistrationRequest("", "example@example.com", "secret1", "secret2")
+        client.post().uri("/register")
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .syncBody(registrationRequest)
+            .exchange()
+            .expectStatus().isBadRequest
+            .expectBody()
+            .jsonPath("$.message").value(`is`("Validation failed"))
+            .jsonPath("$.errors")
+            .value(containsInAnyOrder("'password' and 'confirmPassword' are not matching", "'name' must not be blank"))
     }
 
     @Test
     fun `it should return the created user without the password`() {
         val registrationRequest = RegistrationRequest("name", "example@example.com", "secret", "secret")
-        val resp = mockMvc.perform(
-            post("/register")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content(mapper.writeValueAsString(registrationRequest))
-        )
-            .andExpect(status().isCreated)
-            .andReturn()
+        client.post().uri("/register")
+            .syncBody(registrationRequest)
+            .header("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody()
+            .jsonPath("$.name").value(`is`(registrationRequest.name))
+            .jsonPath("$.email").value(`is`(registrationRequest.email))
+            .jsonPath("$.password").doesNotHaveJsonPath()
+    }
 
-        val returnedUser = mapper.readValue<Map<String, String>>(resp.response.contentAsString)
-        assertTrue(returnedUser.containsKey("name"))
-        assertEquals(registrationRequest.name, returnedUser["name"])
-        assertTrue(returnedUser.containsKey("email"))
-        assertEquals(registrationRequest.email, returnedUser["email"])
-        assertFalse(returnedUser.containsKey("password"))
+    @Test
+    fun `it should not allow registering the same email twice`() {
+        val email = "example@example.com"
+        val registrationRequest = RegistrationRequest("name", email, "secret", "secret")
+        client.post().uri("/register")
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .syncBody(registrationRequest)
+            .exchange()
+            .expectStatus().isCreated
+        client.post().uri("/register")
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .syncBody(registrationRequest)
+            .exchange()
+            .expectStatus().is5xxServerError
+            .expectBody().jsonPath("$.message").isEqualTo("User already exists with email $email")
     }
 }
