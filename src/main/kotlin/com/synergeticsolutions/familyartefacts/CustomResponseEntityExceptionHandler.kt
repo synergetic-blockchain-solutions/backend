a64d27fffb098a7ceaae9dd9b82374692829ef05
Nick Spain <nicholas.spain96@gmail.com>
2019-08-22 21:11:51 +1000

    Add docs to the validation error handler

--- src/main/kotlin/com/synergeticsolutions/familyartefacts/CustomResponseEntityExceptionHandler.kt
+++ src/main/kotlin/com/synergeticsolutions/familyartefacts/CustomResponseEntityExceptionHandler.kt
@@ -9,9 +9,17 @@ import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import java.util.Date
 
+/**
+ * Custom handler for exceptions thrown in request handlers.
+ */
 @ControllerAdvice
 class CustomResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
 
+    /**
+     * Handle exceptions thrown due to request validation errors. This allows us to provide more context than the default
+     * implementation, in that we return a list of the validation errors ("errors" field) rather than just a message
+     * saying that validation has failed.
+     */
     override fun handleMethodArgumentNotValid(
         ex: MethodArgumentNotValidException,
         headers: HttpHeaders,
