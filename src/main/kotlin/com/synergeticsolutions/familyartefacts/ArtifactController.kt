00b4057ea41ef16d916296828a782f72492d03cb
Nick Spain <nicholas.spain96@gmail.com>
2019-09-05 23:14:50 +1000

    Return created HTTP status when artifact is created

--- src/main/kotlin/com/synergeticsolutions/familyartefacts/ArtifactController.kt
+++ src/main/kotlin/com/synergeticsolutions/familyartefacts/ArtifactController.kt
@@ -3,6 +3,7 @@ package com.synergeticsolutions.familyartefacts
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
@@ -69,7 +70,7 @@ class ArtifactController(
             sharedWith = newArtifact.sharedWith ?: listOf()
         )
         logger.info("Created artifact $createdArtifact")
-        return ResponseEntity.ok(createdArtifact)
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdArtifact)
     }
 }
 
