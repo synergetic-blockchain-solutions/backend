40619a8c4b0893f8793221e6fdc22ea5d89f1159
Nick Spain <nicholas.spain96@gmail.com>
2019-09-05 16:35:12 +1000

    Implement artifact creation endpoint

--- src/main/kotlin/com/synergeticsolutions/familyartefacts/ArtifactController.kt
+++ src/main/kotlin/com/synergeticsolutions/familyartefacts/ArtifactController.kt
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -45,4 +47,45 @@ class ArtifactController(
 
         return ResponseEntity.ok(artifacts)
     }
+
+    /**
+     * POST /artifact
+     *
+     * Create an artifact based on the [ArtifactRequest] submitted as the body. In addition to the specified
+     * [ArtifactRequest.owners], the creating user will be made an owner. In addition to the specified
+     * [ArtifactRequest.groups], the creating user's private group will be associated with the artifact.
+     *
+     * @return [Artifact] representing the created artifact
+     */
+    @PostMapping
+    fun createArtifact(@RequestBody newArtifact: ArtifactRequest): ResponseEntity<Artifact> {
+        val currentUser = SecurityContextHolder.getContext().authentication
+        val createdArtifact = artifactService.createArtifact(
+            email = currentUser.principal as String,
+            name = newArtifact.name,
+            description = newArtifact.description,
+            ownerIDs = newArtifact.owners ?: listOf(),
+            groupIDs = newArtifact.groups ?: listOf(),
+            sharedWith = newArtifact.sharedWith ?: listOf()
+        )
+        logger.info("Created artifact $createdArtifact")
+        return ResponseEntity.ok(createdArtifact)
+    }
 }
+
+/**
+ * [ArtifactRequest] represents a request to create an artifact.
+ *
+ * @param [name] Name of the artifact
+ * @param [description] Description of the artifact
+ * @param [owners] User IDs of the users to be made owners of the artifact
+ * @param [groups] Group IDs of the groups to be associated with the artifact
+ * @param [sharedWith] User IDs of the users to share the artifact with
+ */
+data class ArtifactRequest(
+    val name: String,
+    val description: String,
+    val owners: List<Long>?,
+    val groups: List<Long>?,
+    val sharedWith: List<Long>?
+)
