4bbcd01367a0b95af14795db24059aa14d886844
Nick Spain <nicholas.spain96@gmail.com>
2019-09-22 18:30:07 +1000

    Remove unused `AuthenticationException`

--- src/main/kotlin/com/synergeticsolutions/familyartefacts/GroupController.kt
+++ src/main/kotlin/com/synergeticsolutions/familyartefacts/GroupController.kt
@@ -1,6 +1,5 @@
 package com.synergeticsolutions.familyartefacts
 
-import javax.naming.AuthenticationException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
