a9f2b82bafff028cd86da19df68340d83996940b
Nick Spain <nicholas.spain96@gmail.com>
2019-10-10 13:39:19 +1100

    Remove artifact resource tags

--- src/main/kotlin/com/synergeticsolutions/familyartefacts/ArtifactResource.kt
+++ src/main/kotlin/com/synergeticsolutions/familyartefacts/ArtifactResource.kt
@@ -2,14 +2,14 @@ package com.synergeticsolutions.familyartefacts
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
-import org.hibernate.annotations.LazyCollection
-import org.hibernate.annotations.LazyCollectionOption
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.Lob
 import javax.persistence.ManyToOne
+import org.hibernate.annotations.LazyCollection
+import org.hibernate.annotations.LazyCollectionOption
 
 /**
  * Resource associated with an [Artifact].
