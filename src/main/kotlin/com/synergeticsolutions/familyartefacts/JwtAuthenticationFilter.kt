0a1b5e1e13706d52a5d4ac15dcaa6a930fd219e1
Nick Spain <nicholas.spain96@gmail.com>
2019-10-11 13:50:35 +1100

    Fix imports

--- src/main/kotlin/com/synergeticsolutions/familyartefacts/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/synergeticsolutions/familyartefacts/JwtAuthenticationFilter.kt
@@ -2,14 +2,16 @@ package com.synergeticsolutions.familyartefacts
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import javax.servlet.FilterChain
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 class JwtAuthenticationFilter(val authManager: AuthenticationManager, loginUrl: String, val tokenService: TokenService) : UsernamePasswordAuthenticationFilter() {
     init {
