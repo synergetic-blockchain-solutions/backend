aea1fade4fd23e75e810b2d0f07ad30c8abe0aac
Nick Spain <nicholas.spain96@gmail.com>
2019-10-11 13:32:16 +1100

    Remove prefix for user controller

--- src/main/kotlin/com/synergeticsolutions/familyartefacts/UserController.kt
+++ src/main/kotlin/com/synergeticsolutions/familyartefacts/UserController.kt
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping
 class UserController(
     @Autowired
     val userService: UserService
