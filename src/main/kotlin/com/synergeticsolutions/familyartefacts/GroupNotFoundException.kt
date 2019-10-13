a10f36851d4baf633e01a6e7e66889553c3df3b6
Nick Spain <nicholas.spain96@gmail.com>
2019-10-14 02:23:10 +1100

    Map GroupNotfoundException to a 404

--- src/main/kotlin/com/synergeticsolutions/familyartefacts/GroupNotFoundException.kt
+++ src/main/kotlin/com/synergeticsolutions/familyartefacts/GroupNotFoundException.kt
@@ -1,3 +1,7 @@
 package com.synergeticsolutions.familyartefacts
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class GroupNotFoundException(msg: String) : RuntimeException(msg)
