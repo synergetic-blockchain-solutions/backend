512c861a4b5cb1c50b24f494ea6b3155da8ad7f5
Nick Spain <nicholas.spain96@gmail.com>
2019-10-26 23:58:12 +1100

    Fix error in tests

--- src/test/kotlin/com/synergeticsolutions/familyartefacts/ArtifactControllerTest.kt
+++ src/test/kotlin/com/synergeticsolutions/familyartefacts/ArtifactControllerTest.kt
@@ -8,6 +8,7 @@ import com.synergeticsolutions.familyartefacts.dtos.LoginRequest
 import com.synergeticsolutions.familyartefacts.dtos.LoginResponse
 import com.synergeticsolutions.familyartefacts.entities.Album
 import com.synergeticsolutions.familyartefacts.entities.Artifact
+import com.synergeticsolutions.familyartefacts.entities.ArtifactResource
 import com.synergeticsolutions.familyartefacts.entities.Group
 import com.synergeticsolutions.familyartefacts.entities.User
 import com.synergeticsolutions.familyartefacts.repositories.AlbumRepository
@@ -410,7 +411,8 @@ class ArtifactControllerTest {
                 owners = listOf(),
                 groups = listOf(group.id),
                 sharedWith = listOf(user2.id),
-                tags = listOf("test1", "test2")
+                tags = listOf("test1", "test2"),
+                albums = listOf(album.id)
             )
             val response = client.post()
                     .uri("/artifact")
